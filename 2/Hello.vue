823f7363a8879d539ebc4c8f89c99f12f7fb9a3a
Ahmed Moussa <ahmsayat@users.noreply.github.com>
2020-05-13 05:02:41 -0700

    Create Hello.vue

---
+++ 2/Hello.vue
@@ -0,0 +1,21 @@
+  
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>  
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue World!'
+    }
+  })
+</script>  
+
+<style>
+  p {
+    font-size: 2em;
+  }
+</style>
